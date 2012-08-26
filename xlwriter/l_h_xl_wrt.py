346c33e72a62a5c3380d7391e828629fbe64fbc8
czh <chsc4698@gmail.com>
2012-08-26 19:53:27 +0800

    bug fixed

--- xlwriter/l_h_xl_wrt.py
+++ xlwriter/l_h_xl_wrt.py
@@ -1,11 +1,10 @@
 # encoding: utf-8
-from win32com.client import Dispatch
 from libs import misc, config
-from libs.sqlite_writer import SQLiteOperator
 from libs.xls_oprt import ExcelOperator
+from xlwriter.abstract_wrt import AbstractXLWriter
 
 
-class LaborHourDataXLWriter(object):
+class LaborHourDataXLWriter(AbstractXLWriter, object):
     LH_OFFSET = 3
     WASTE_OFFSET = 3
 
@@ -16,13 +15,8 @@ class LaborHourDataXLWriter(object):
     INSERT_A = 0x3
 
     def __init__(self, sheet_index, base_name, xl_out_path, app_echo=True, save_per_worker=False):
+        super(LaborHourDataXLWriter, self).__init__(xl_out_path, app_echo, save_per_worker)
         self.base_name = base_name
-
-        self.app_echo = app_echo
-
-        self.xl_app = Dispatch('Excel.Application')
-        self.xl_app.Visible = app_echo
-        self.xl_app.Workbooks.Open(xl_out_path)
 
         self.lh_sheet = self.xl_app.Sheets((base_name + config.lh_suffix).decode('utf-8').encode('gbk'))
         self.w_sheet = self.xl_app.Sheets((base_name + config.w_suffix).decode('utf-8').encode('gbk'))
@@ -31,14 +25,14 @@ class LaborHourDataXLWriter(object):
         xl_operator = ExcelOperator(config.XLS_PATH)
         self.worker_dict_x = dict(xl_operator.get_id_name_pairs_with_row_number(sheet_index))
         self.main_worker_dict_x = dict(xl_operator.get_id_name_pairs_with_row_number(0))
-        self.db_operator = SQLiteOperator()
 
-        self.save_per_worker = save_per_worker
+        self.write_dates()
 
 
-    def _echo_sheet(self, sheet, worker_row):
-        sheet.Select()
-        sheet.Range('A%s' % worker_row).Select()
+    def write_dates(self):
+        for col_cnt, day in enumerate(self.db_operator.get_month_days_in_db()):
+            self.lh_sheet.Cells(3, LaborHourDataXLWriter.LH_OFFSET + col_cnt).Value = day
+            self.w_sheet.Cells(3, LaborHourDataXLWriter.WASTE_OFFSET + col_cnt).Value = day
 
 
     def write_data_per_worker(self,
@@ -71,21 +65,18 @@ class LaborHourDataXLWriter(object):
             for __, (__, __, __, day_assist) in cache:
                 assist_sum += day_assist
             if self.app_echo:
-                self._echo_sheet(self.m_sheet, worker_row)
+                self._echo_sheet(self.m_sheet, self.main_worker_dict_x[worker_id][1])
             self.m_sheet.Cells(self.main_worker_dict_x[worker_id][1],
                                LaborHourDataXLWriter.ASSIST_COL).Value = assist_sum if assist_sum else ''
-
 
         if self.save_per_worker:
             self.xl_app.ActiveWorkbook.Save()
 
 
     def write_all_data(self):
-        for worker_id, (worker_name, worker_row) in misc.sort_dict_keys_numerically(self.worker_dict_x):
+        for worker_id, (_, worker_row) in misc.sort_dict_keys_numerically(self.worker_dict_x):
             self.write_data_per_worker(worker_id, worker_row)
 
-        if not self.save_per_worker:
-            self.xl_app.ActiveWorkbook.Save()
-
+        super(LaborHourDataXLWriter, self).write_all_data()
 
 
