a0dd316353aba945736ce4c3aa7c9887e30899e4
czh <chsc4698@gmail.com>
2012-08-26 20:00:13 +0800

    added support for PerformanceXLWriter

--- xlwt_bootstrap.py
+++ xlwt_bootstrap.py
@@ -1,10 +1,15 @@
 # encoding: utf-8
 import os
 from libs import config
-from xlwriter.l_h_d_wrt import LaborHourDataXLWriter
+from xlwriter.l_h_xl_wrt import LaborHourDataXLWriter
+from xlwriter.p_xl_wrt import PerformanceXLWriter
 
 
 if __name__ == '__main__':
     for base_name, index in config.short_title_index_pairs:
         writer = LaborHourDataXLWriter(index, base_name, os.path.abspath('data_copy.xls'))
         writer.write_all_data()
+
+        writer = PerformanceXLWriter(index, base_name, os.path.abspath('data_copy.xls'))
+        writer.write_all_data()
+
